5612758f5f7bd6a72f6b6d9922484327803865e4
gaoxiag <gx@m8-cbg-ipu-819310374.ops.megvii-inc.com>
2022-04-23 23:21:23 +0800

    add row share memory

--- cuda/MMult_cuda_2.cu
+++ cuda/MMult_cuda_2.cu
@@ -9,11 +9,11 @@
 /**
  * naive 实现
  */
-template <int BLOCK>
+ 
 __global__ void sgemm(int m, int n, int k, float *a, int lda, float *b, int ldb,
                       float *c, int ldc) {
-  int _m = blockIdx.x * BLOCK + threadIdx.x;
-  int _n = blockIdx.y * BLOCK + threadIdx.y;
+  int _m = blockIdx.x * blockDim.x + threadIdx.x;
+  int _n = blockIdx.y * blockDim.y + threadIdx.y;
   if (_m < m and _n < n) {
     float sum = 0.f;
     for (int i = 0; i < k; ++i) {
@@ -31,5 +31,5 @@ void MY_MMult(cublasHandle_t handle, int m, int n, int k, float *d_A, int lda,
   dim3 block(BLOCK, BLOCK);
   dim3 grid((m + BLOCK - 1) / BLOCK, (n + BLOCK - 1) / BLOCK);
 
-  sgemm<BLOCK><<<grid, block>>>(m, n, k, d_A, lda, d_B, ldb, d_C, ldc);
+  sgemm<<<grid, block>>>(m, n, k, d_A, lda, d_B, ldb, d_C, ldc);
 }
